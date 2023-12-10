57d77c267f4cad2eb7e537b879a1be897310a5fd
SepehrFci <sepehrfci@gmail.com>
2023-12-10 15:35:21 +0330

    add fonts and edit add-csv-file.html file

--- app.py
+++ app.py
@@ -5,11 +5,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    
-@app.route('/css/style.css')
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/add-csv-file')
+def add_csv_file():
+    return render_template('add-csv-file.html')  
+
+
+
+@app.route('/static/style.css')
 def style():
   return send_file("static/style.css")
-    
+@app.route('/static/font.ttf')
+def font():
+  return send_file("static/Pinar-SemiBold.ttf") 
     
 # class user :
 #     def __init__(self,username,serverName,password,expirationDate) :
